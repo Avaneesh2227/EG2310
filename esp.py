f2c2e8d94564a3b8027c5aee8cb733141b901f0c
Avaneesh <79705938+Avaneesh2227@users.noreply.github.com>
2024-06-10 15:34:04 +0530

    Create esp.py

---
+++ esp.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def door():
+    url = 'http://192.168.59.89/openDoor'
+    myobj = {
+            "action": "openDoor",
+            "parameters": {"robotId": "34"}
+            }
+    x = requests.post(url, json = myobj)
+    x_dict = json.loads(x.text)
+    status = x_dict["status"]
+    door_num = x_dict["data"]["message"]
+    return door_num
+
+print(door())
